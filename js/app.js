765fa840307a596cdb4fe912763c2e9e3fab90c7
wimaks <squ1rtle.mg@gmail.com>
2023-03-20 21:18:05 +0600

    Закончил адаптив

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    //* BURGER ACTIVE
+
+    $('.nav-toggle').on('click', function (event) {
+        event.preventDefault();
+
+        $('.burger').toggleClass('active'); // появление меню при нажатии на бургер
+        $(this).toggleClass('active'); // изменение меню бургер при открытии меню
+    });
+});
